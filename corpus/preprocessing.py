f1a019ae6b3c9ae7aa716fd66eb571918f42740c
Song Yang <songyang1218@gmail.com>
2020-10-28 22:39:44 -0400

    Default aggressive preprocessor

--- corpus/preprocessing.py
+++ corpus/preprocessing.py
@@ -6,7 +6,10 @@ Sets up a preprocessing pipeline
 from typing import Callable, Iterator, List, Tuple
 import os
 import pandas as pd
+import argparse
+
 from pandas.core.frame import DataFrame
+
 
 class Labels(object):
     """Represents the labels of a data point (a blog)
@@ -53,7 +56,7 @@ class Preprocessor(object):
         self.preprocesses : List[Callable[[List[str]], List[str]]] = []
         self.tokenizer: Callable[[str], List[str]] = lambda x : x.split()
 
-    def blog_stream(self) -> Iterator[Tuple[str, Labels]]:
+    def blog_stream(self, return_unparsed_labels: bool = False):
         """Iterates over the corpus directly from disk
         """
         for path in os.listdir(self.path_to_corpus_directory):
@@ -62,7 +65,28 @@ class Preprocessor(object):
             dataframe = pd.read_csv(os.path.join(self.path_to_corpus_directory, path),\
                 names=('ID', 'Gender', 'Age', 'Zodiac', 'Blog'))
             for row in dataframe['Blog']:
-                yield row, Labels(dataframe['Gender'][0], dataframe['Age'][0], dataframe['Zodiac'][0])
+                if return_unparsed_labels:
+                    yield row, dataframe['ID'][0], dataframe['Gender'][0], dataframe['Age'][0], dataframe['Zodiac'][0]
+                else:
+                    yield row, Labels(dataframe['Gender'][0], dataframe['Age'][0], dataframe['Zodiac'][0])
+
+    def save(self, directory: str):
+        """Saves the preprocessed corpus to the directory
+
+        Args:
+            directory (str): Folder where to save
+        """
+        import shutil
+        shutil.rmtree(directory)
+        os.makedirs(directory)
+        for blog, id, gender, age, zodiac in self.blog_stream(True):
+            preprocessed = self.tokenizer(blog)
+            for preprocess in self.preprocesses:
+                preprocessed = preprocess(preprocessed)
+            dataframe = DataFrame([[id, gender, age, zodiac, " ".join(preprocessed)]])
+            with open(os.path.join(directory, "{id}.{gender}.{age}.{zodiac}.csv".format(id=id, gender=gender, age=age, zodiac=zodiac)), 'a+') as file:
+                dataframe.to_csv(file, header=False, index=False, mode='a+', line_terminator='\n')
+
 
     def run(self) -> List[Tuple[List[str], Labels]]:
         """
@@ -86,17 +110,47 @@ class Preprocessor(object):
             yield preprocessed, blogger
 
 def main():
-    preprocessor = Preprocessor("data/excerpt")
+    parser = argparse.ArgumentParser("Runs the full preprocessing pipeline")
+
+    parser.add_argument("corpus", help="Where the corpus is located")
+
+    parser.add_argument("save", metavar="save-location" , help="Where to save")
+
+    args = parser.parse_args()
+
+    preprocessor = Preprocessor(args.corpus)
     # choose a tokenizer, by default it is python's split
-    # preprocessor.tokenizer = nltk.tokenize # a function
+    from nltk.tokenize import TweetTokenizer
+    tweet = TweetTokenizer(preserve_case=False, reduce_len=True)
+    preprocessor.tokenizer = tweet.tokenize # a function
 
     # append as many non-tokenizing preprocesses as needed, e.g. lowercase mapping
-    # preprocessor.preprocesses.append( lambda x: [word.lower() for word in x] )
+    # fn( List[str] ) -> List[str]
+
+    def map_numbers(words: List[str]):
+        for word in words:
+            yield 'NUM' if word.isnumeric() else word
+
+    def map_nonascii(words: List[str]):
+        for word in words:
+            yield 'NONASCII' if not word.isascii() else word
+
+    # stemming is too weird
+    # from nltk.stem import PorterStemmer
+    # stemmer = PorterStemmer()
+
+    preprocessor.preprocesses.extend([
+        # lowercase is handled by the tweet tokenizer
+        # numbers
+        lambda x: list(map_numbers(x)),
+        # nonascii
+        lambda x: list(map_nonascii(x))
+        # stemming
+        #lambda x: [stemmer.stem(word) for word in x]
+        ])
 
     # run the preprocessing pipeline
-    corpus = preprocessor.run()
-    for blog, blogger in corpus:
-        print(blog[:5], str(blogger))
+    preprocessor.save(args.save)
         
 
 if __name__ == "__main__":
